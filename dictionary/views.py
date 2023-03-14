1d6c658580749b38ee021f95614668aa961156e3
only.zabao <ntgbao.235@gmail.com>
2023-03-15 03:11:53 +0700

    (test) using html file for response

--- dictionary/views.py
+++ dictionary/views.py
@@ -12,6 +12,19 @@ def search(request):
     query = request.GET.get('q')
     if query:
         results = get_word_data(query, "c6f0f020-6846-46e7-bcc6-3fab213e84c8")
-        return JsonResponse(results, safe=False)
+        definitions = []
+        for result in results:
+            if 'meta' in result and 'id' in result['meta']:
+                definition = {
+                    'word': result['meta']['id'],
+                    'part_of_speech': result['fl'],
+                    'definition': result['shortdef'][0],
+                }
+                if 'sseq' in result and result['sseq'][0][0][0] == 'sense':
+                    examples = result['sseq'][0][0][1].get('examples')
+                    if examples:
+                        definition['examples'] = [example['t'] for example in examples]
+                definitions.append(definition)
+        return render(request, 'search.html', {'definitions': definitions, 'query': query})
     else:
         return render(request, 'search.html')
