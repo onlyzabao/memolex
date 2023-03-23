12ced05b48eebfdc890d6a9204b51b9c8d5c6caa
only.zabao <ntgbao.235@gmail.com>
2023-03-23 23:51:09 +0700

    add workspace app

--- MemoLex/settings.py
+++ MemoLex/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dictionary',
+    'workspace',
+    'community',
     'debug_toolbar',
 ]
 
---
+++ workspace/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Package(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    PRIVACY_CHOICES = [
+        (0, 'Private'),
+        (1, 'Public')
+    ]
+    privacy = models.SmallIntegerField(choices=PRIVACY_CHOICES, default=1)
+
+class User_Package(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    package = models.ForeignKey(Package, on_delete=models.CASCADE)
+    progress = models.SmallIntegerField()
+    STAR_CHOICES = [
+        (0, 'Not started'),
+        (1, 'Remembered'),
+        (2, 'Understood'),
+        (3, 'Applying')
+    ]
+    star = models.SmallIntegerField(choices=STAR_CHOICES, default=0)
+
+class Word_Package(models.Model):
+    package = models.ForeignKey(Package, on_delete=models.CASCADE)
+    word = models.CharField(max_length=50)
+    uuid = models.CharField(max_length=50)
+    sn = models.CharField(max_length=10)
